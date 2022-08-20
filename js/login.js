677b864debe1dcac11e1c28ebc5505104875d82f
emilianolopez3 <emimazzilli11@gmail.com>
2022-08-20 15:37:15 -0300

    c

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const email = document.getElementById("email");
+const contrasenia = document.getElementById('password');
+
+function redirect() {
+    location.assign("http://127.0.0.1:5500/index.html")
+}
+
