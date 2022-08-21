868ecc52418eb66051c7e6fb52ce6dad64cb2935
emilianolopez3 <emimazzilli11@gmail.com>
2022-08-21 14:10:25 -0300

    r

--- js/login.js
+++ js/login.js
@@ -1,7 +1,16 @@
 const email = document.getElementById("email");
 const contrasenia = document.getElementById('password');
+const button = document.getElementById("regBtn");
+
+
+
+
+
+
+
+
+
 
 function redirect() {
-    location.assign("http://127.0.0.1:5500/index.html")
+    window.location.href = ("http://127.0.0.1:5500/index.html")
 }
-
