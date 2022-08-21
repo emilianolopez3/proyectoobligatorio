b76a9a6852fbee65e2e90b5cd2475314ef8805d7
emilianolopez3 <emimazzilli11@gmail.com>
2022-08-21 16:56:40 -0300

    j

--- js/login.js
+++ js/login.js
@@ -1,8 +1,13 @@
-const email = document.getElementById("email");
-const contrasenia = document.getElementById('password');
+
 const button = document.getElementById("regBtn");
 
+document.getElementById(regBtn).addEventListener("click", function () ) {
+       var email = document.getElementById("email");
+    var contrasenia = document.getElementById('password'); 
+
+if ((email.value =="")) 
 
 function redirect() {
     window.location.href = "https://emilianolopez3.github.io/proyectoobligatorio/"
+ }
 }
