8fd4565727d267556e70d7b33c248bd05ea24d6d
emilianolopez3 <emimazzilli11@gmail.com>
2022-08-21 16:45:07 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,14 +3,6 @@ const contrasenia = document.getElementById('password');
 const button = document.getElementById("regBtn");
 
 
-
-
-
-
-
-
-
-
 function redirect() {
-    window.location.href = ("http://127.0.0.1:5500/index.html")
+    window.location.href = "https://github.com/emilianolopez3/proyectoobligatorio"
 }
