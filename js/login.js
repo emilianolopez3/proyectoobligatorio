9f6e74e96cd62054a8f9f472cca039d60265bf77
emilianolopez3 <emimazzilli11@gmail.com>
2022-09-02 21:11:33 -0300

    consegui

--- js/login.js
+++ js/login.js
@@ -2,10 +2,8 @@ function redireccionar(){
     var email = document.getElementById("email").value;
     var contrasenia = document.getElementById('password').value;
     if ( email != "" && contrasenia != "") {
-        alert("entro aqui")
-        location.href = "index.html"
+       location.href="index.html"
     }else{
-         alert("Debe ingreser email y contraseña")  
+         alert("Debe ingreser email y contraseña")
         }   
 }
-
