946f3389db4281dd4de65516ae59e64135e382a4
emilianolopez3 <emimazzilli11@gmail.com>
2022-09-03 21:51:54 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,7 +2,6 @@ function redireccionar() {
    var email = document.getElementById("email").value;
    var contrasenia = document.getElementById('password').value;
    if (email != "" && contrasenia != "") {
-      localStorage.setItem("email", email);
       location.href = "index.html";      
    } else {  
       alert("Debe ingreser email o contraseña")
